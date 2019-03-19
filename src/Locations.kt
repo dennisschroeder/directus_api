5af6a2c08fed4b5216746bf325ed2730a4977aab
dennisschroeder <dennisschroeder@me.com>
2019-03-19 10:35:36 +0100

    [TASK] Introduces location feature

--- src/Locations.kt
+++ src/Locations.kt
@@ -1,2 +1,9 @@
 package com.directus
 
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+
+@KtorExperimentalLocationsAPI
+
+@Location("/reset/{resetToken}")
+data class ResetPassword(val resetToken: String)
